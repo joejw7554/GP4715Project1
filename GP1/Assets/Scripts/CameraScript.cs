2fe0cd14210c1b62c3a29ce98b9d5dbb1c643262
JaeWoo Joe <joejw7554@knights.ucf.edu>
2022-01-23 01:47:11 -0500

    Mario Jump

---
+++ GP1/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, this.transform.position.z);
+    }
+}
--- GP1/Assets/Scripts/PlayerScript.cs
+++ GP1/Assets/Scripts/PlayerScript.cs
@@ -4,23 +4,56 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    private Rigidbody2D rd2d;
-    public float speed;
-
     // Start is called before the first frame update
-
+    public float speed;
+    public int JumpPower=1250;
+    private bool facingRight= true;
+    private float hozMove;
     void Start()
     {
-        rd2d= GetComponent<Rigidbody2D>();
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float hozMovement= Input.GetAxis("Horizontal");
-        float verMovement= Input.GetAxis("Vertical");
 
-        rd2d.AddForce(new Vector2(hozMovement*speed, verMovement*speed));
+    }
+    void FixedUpdate()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        if(Input.GetButtonDown ("Jump"))
+        {      
+            Jump();
+        }
+
+        hozMove= Input.GetAxis("Horizontal");
+        
+        if(hozMove<0.0f && facingRight==true)
+        {
+            FlipPlayer();
+        }
+        else if(hozMove>0.0f && facingRight==false)
+        {
+            FlipPlayer();
+        }
+
+        gameObject.GetComponent<Rigidbody2D>().velocity =new Vector2 (hozMove*speed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up*JumpPower);
+    }
+    void FlipPlayer()
+    {
+        facingRight=! facingRight;
+        Vector2 localScale =gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale=localScale;
+
     }
 }
